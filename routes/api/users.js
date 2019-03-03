94340c91ac9d5f4cb33cdf53b4a6e61656d3ac4f
Natalie <natalieschennum@gmail.com>
2019-03-03 13:08:48 -0800

    Adding Register and Login Vallidators

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,11 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys');
+const passport = require('passport'); 
+
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 const User = require("../../models/User");
 
@@ -14,9 +19,16 @@ router.get("/test", (req, res) => res.json({ msg: "Users works!" }));
 // with mongoose you can use callbacks or promises
 //.then is using promises
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = 'Email already exists.'
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
@@ -46,6 +58,12 @@ router.post("/register", (req, res) => {
 
 // User Login /Returns JWT
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email; 
   const password = req.body.password;
 
@@ -54,7 +72,8 @@ router.post('/login', (req, res) => {
   .then(user => {
     //check for user
     if(!user) {
-      return res.status(404).json({email: 'User not found'})
+      errors.email = 'User not found'
+      return res.status(404).json(errors)
     }
 
     //Check password
@@ -65,18 +84,31 @@ router.post('/login', (req, res) => {
 
         const payload = { id: user.id, name: user.name, avatar: user.avatar } // payload
         //Sign Token
-        jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
           res.json({
             success: true, 
             token: 'Bearer ' + token
           })
         });
       } else {
-        return res.status(400).json({password: 'Incorrect Password'})
+        errors.password = 'Password incorrect'
+        return res.status(400).json(errors)
       }
     })
   })
 })
 
+// Return current user private routes
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
