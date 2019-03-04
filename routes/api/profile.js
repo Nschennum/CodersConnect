86a13e7c55f72794f412d9e0e1223483fc9e9b97
Natalie <natalieschennum@gmail.com>
2019-03-04 10:18:05 -0800

    DELETE s for Ex p and Ed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -196,4 +196,53 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req, 
   })
 });
 
+// DELETE api/profile/experience/:exp_id
+router.post('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    // GET remove index
+    const removeIndex = profile.experience
+    .map(item => item.id)
+    .indexOf(req.params.exp_id);
+
+    // Splice out of array
+    profile.experience.splice(removeIndex, 1);
+
+    // Save
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err))
+});
+
+// DELETE api/profile/education/:edu_id
+router.post('/education/:edu_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    // GET remove index
+    const removeIndex = profile.education
+    .map(item => item.id)
+    .indexOf(req.params.exp_id);
+
+    // Splice out of array
+    profile.education.splice(removeIndex, 1);
+
+    // Save
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err))
+});
+
+// DELETE User and Profile api/profile
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile.findOneAndRemove({ user: req.user.id })
+  .then(() => {
+    User.findOneAndRemove({ _id: req.user.id })
+    .then(() => res.json({ sucess: true}))
+  })
+  .catch(err => res.status(404).json(err))
+});
+
 module.exports = router;
