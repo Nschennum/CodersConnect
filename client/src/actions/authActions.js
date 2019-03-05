7371eacaa5778f52cb5811e36d18e9994ad68c2c
Natalie <natalieschennum@gmail.com>
2019-03-05 12:41:23 -0800

    Started Login Redux functionality

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,7 +5,27 @@ import { GET_ERRORS } from "./types";
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("api/users/register", userData)
-    .then(res => history.push('/login'))
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Login -GET User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //Save t local storage
+      const { token } = res.data;
+      //set token to local storage
+      localStorage.setItem('jwtToken', token); 
+      //set token to auth header
+      setAuthToken(token);
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
