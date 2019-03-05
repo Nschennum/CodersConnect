22f92d894e5a6c6be0855e3e795df5fc937c1326
Natalie <natalieschennum@gmail.com>
2019-03-05 10:43:06 -0800

    Testing Register with  Axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import classnames from 'classnames';
+import { connect } from 'react-redux'; 
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -27,11 +31,22 @@ onSubmit = (e) => {
     password2: this.state.password2
   }
 
-  console.log(newUser);
+  this.props.registerUser(newUser); 
+
+    // axios
+    // .post('api/users/register', newUser)
+    // .then(res => console.log(res.data))
+    // .catch(err => this.setState({errors: err.response.data}))
 }
   render() {
+
+    const { errors } = this.state; //aka const errors = this.state.errors 
+
+    const { user } = this.props.auth
+
     return (
       <div className="register">
+      {user ? user.name : null }
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -43,17 +58,17 @@ onSubmit = (e) => {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {'is-invalid': errors.name})}
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
                     onChange={this.onChange}
-                  />
+                  />{errors.name && (<div className='invalid-feedback'>{errors.name}</div>)}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {'is-invalid': errors.email})}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
@@ -62,27 +77,27 @@ onSubmit = (e) => {
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
-                  </small>
+                  </small>{errors.email && (<div className='invalid-feedback'>{errors.email}</div>)}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {'is-invalid': errors.password})}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
-                  />
+                  />{errors.password && (<div className='invalid-feedback'>{errors.password}</div>)}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {'is-invalid': errors.password2})}
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
-                  />
+                  />{errors.password2 && (<div className='invalid-feedback'>{errors.password2}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -94,4 +109,8 @@ onSubmit = (e) => {
   }
 }
 
-export default Register;
+const mSTP = (state) => ({
+  auth: state.auth //must match rootReducer in reducer index
+})
+
+export default connect(mSTP, { registerUser })(Register);
