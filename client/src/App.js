ffa752be0ebf863876a7449bcac0c23f14e15045
Natalie <natalieschennum@gmail.com>
2019-03-05 12:07:42 -0800

    Initiated Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { Provider } from 'react-redux'; 
+import store from './store'; 
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import './App.css';
+
+
+
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
       <Navbar/>
@@ -21,6 +28,7 @@ class App extends Component {
        <Footer/> 
       </div>
       </Router>
+      </Provider>
     );
   }
 }
