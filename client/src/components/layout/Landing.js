c464835d2b6403d31404d95db628243cdce30ce3
Natalie <natalieschennum@gmail.com>
2019-03-06 15:01:38 -0800

    Updated landing to push to dashboard with auth

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types'; 
+import { connect } from 'react-redux';
 
 class Landing extends Component {
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
   render() {
     return (
       <div className="landing">
@@ -24,6 +32,13 @@ class Landing extends Component {
   }
 }
 
+Landing.propTypes ={
+  auth: PropTypes.object.isRequired
+}
+
+const mSTP = (state) => ({
+  auth: state.auth, //must match rootReducer in reducer index
+})
 
 
-export default Landing;
+export default connect(mSTP)(Landing);
