88c262c1e6ee6a80bc75bd4d78ff69ac65500381
Natalie <natalieschennum@gmail.com>
2019-03-14 11:00:54 -0700

    Prepared for Deploy

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
 
-const users = require('./routes/api/users'); 
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
-const app = express(); 
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // mongoose.models = {};
 // mongoose.modelSchemas = {};
 // Connect to MongoDB
 mongoose
-.connect(db, { useNewUrlParser: true, useFindAndModify: false } )
-  .then(() => console.log('MongoDB Connected'))
+  .connect(db, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-  //passport middleware
+//passport middleware
 app.use(passport.initialize());
 
 //passport config
-require('./config/passport')(passport); 
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users); 
-app.use('/api/profile', profile); 
-app.use('/api/posts', posts); 
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000; 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder to frontend html
+  app.use(exress.static("client/build"));
 
-app.listen(port, () => console.log(`Server running on port ${port}`)); 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
